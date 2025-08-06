99e808c0a592f2ae68892e683a2c1291a940590d
rahuljhrj10 <3jharahul10@gmail.com>
2025-08-07 00:25:42 +0530

    initialize Spring Boot project with Gradle setup and essential configurations

---
+++ src/main/java/com/namarahul/namarahulcollab/NamarahulcollabApplication.java
@@ -0,0 +1,13 @@
+package com.namarahul.namarahulcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamarahulcollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamarahulcollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namarahul/namarahulcollab/NamarahulcollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namarahul.namarahulcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamarahulcollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
